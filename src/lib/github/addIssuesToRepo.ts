212b4a49bcb550e9677e0d3c13e4d05c64c1350f
lubega simon <lubegasimonwalker@gmail.com>
2018-10-13 13:13:32 +0300

    updated alignment (#42)

--- src/lib/github/addIssuesToRepo.ts
+++ src/lib/github/addIssuesToRepo.ts
@@ -13,7 +13,7 @@ export interface Issue {
 export default async function addIssuesToRepo(context: Context, issues: Issue[] ): Promise<any[]> {
   const octokit = context.github;
   const { owner, repo } = getBasicRepoProps (context);
-    const promises: Array<Promise<any>> = issues.map( async (issue: Issue) => {
+  const promises: Array<Promise<any>> = issues.map( async (issue: Issue) => {
       const title = issue.title;
       const body = issue.body;
       const fields = {
@@ -22,7 +22,7 @@ export default async function addIssuesToRepo(context: Context, issues: Issue[] 
         title,
         body
       };
-     return await octokit.issues.create(fields);
+      return await octokit.issues.create(fields);
     });
   return Promise.all(promises);
 }
