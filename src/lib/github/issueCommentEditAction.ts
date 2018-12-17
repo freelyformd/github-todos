27a59a846a2fbcbe3348188bc3a3ae714029e866
lubegasimon <lubegasimonwalker@gmail.com>
2018-12-17 21:47:27 +0300

    function that enables an issue comment to be editable

---
+++ src/lib/github/issueCommentEditAction.ts
@@ -0,0 +1,39 @@
+/**
+ * function that enables an issue comment to be editable, that is to say,
+ * provides the delete and edit(overwrite) options.
+ *
+ * @param: context & issueCommentArray
+ * @returns: Promise<void>
+ */
+
+import { getBasicRepoProps } from "./utils";
+import { Context } from "./types";
+
+type CommentEditAction = "Delete" | "Edit";
+
+const Delete: CommentEditAction = "Delete";
+const Edit: CommentEditAction = "Edit";
+
+interface CommentEdit {
+  GhIssueId: number;
+  GhCommentId: number;
+  action: CommentEditAction;
+  commentsText: string[];
+}
+
+type CommentsEdit = CommentEdit[];
+
+export default async function issueCommentToEdit (context: Context, issueComment: CommentsEdit): Promise<void> {
+    const octokit = context.github;
+    const { owner, repo } = getBasicRepoProps(context);
+    issueComment.map(async (comment) => {
+        // const issueId = await octokit.issues.get({owner, repo, number: comment.GhIssueId});
+        const commentId = await octokit.issues.getComment({owner, repo, comment_id: comment.GhCommentId});
+        if (comment.action === Delete) {
+            return await octokit.issues.deleteComment({owner, repo, comment_id: commentId});
+        }
+        if (comment.action === Edit) {
+            return await octokit.issues.updateComment({owner, repo, comment_id: commentId, body: comment.commentsText});
+        }
+    });
+}
--- src/lib/github/issueCommentsToEdit.ts
+++ src/lib/github/issueCommentsToEdit.ts
@@ -1,4 +1,10 @@
-// remove checked out or no longer existing issueComment from bot created issuesList
+/**
+ * function to return an array of issue comments to edit (overwrite) or delete, that is to say,
+ * checked out or no longer existing issue comments in source code.
+ *
+ * @param: context & issueList
+ * @returns: Promise<void>
+ */
 
 import getChangedFiles from "./changedFilesList";
 import getFilesContent from "./getFilesContent";
@@ -10,18 +16,14 @@ export default async function removeIssue (context: Context, issueList: RawGHIss
     const changedFiles = await getChangedFiles(context);
     const filesContent = await getFilesContent(changedFiles);
 
-    comments.forEach(issueComment => {
-        const issueCommentIndex = comments.indexOf(issueComment);
-        return filesContent.forEach(fileContent => {
+    comments.forEach(async issueComment => {
+        const issueCommentsToEdit = [];
+        return filesContent.forEach(async fileContent => {
             fileContent.content.includes(issueComment) || !issueComment.includes("[x]")
             ? issueComment === comments[comments.length - 1]
                 ? issueList
                 : comments.concat([issueComment]).join("\n")
-            : issueCommentIndex > -1
-                ? issueComment === comments[comments.length - 1]
-                    ? comments.splice(issueCommentIndex, 1).join("\n")
-                    : comments.splice(issueCommentIndex, 1)
-                : issueList;
+            : issueCommentsToEdit.push(issueComment);
         });
     });
 }
